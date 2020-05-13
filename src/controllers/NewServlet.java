25f3edacad6da4d33532fcb738dba37aaec7b160
yui.kondoh <yui.kondo@axas-japan.co.jp>
2020-05-13 15:28:07 +0900

    Add NewServlet.java

---
+++ src/controllers/NewServlet.java
@@ -0,0 +1,59 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class NewServlet
+ */
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+        em.getTransaction().begin();
+
+        // Taskのインスタンスを生成
+        Task t = new Task();
+
+        // ｔのプロパティにデータを代入
+        String content = "掃除";
+        t.setContent(content);
+
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());     // 現在の日時を取得
+        t.setCreated_at(currentTime);
+        t.setUpdated_at(currentTime);
+
+        // データベースに保存
+        em.persist(t);
+        em.getTransaction().commit();
+
+        // 自動採番されたIDの値を表示
+        response.getWriter().append(Integer.valueOf(t.getId()).toString());
+
+        em.close();
+    }
+
+}
